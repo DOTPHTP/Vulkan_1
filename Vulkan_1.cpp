31f381cd276ddb142c820fa2433afd19563155ff
DOTPHTP <1423701360@qq.com>
2025-03-31 18:38:04 +0800

    更新部分组件

--- Vulkan_1.cpp
+++ Vulkan_1.cpp
@@ -1,35 +1,124 @@
 ﻿#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
-
 #include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+#include <vector>
+#include <cstring>
+class HelloTriangleApplication {
+public:
+	void run() {
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	};
 
-int main() {
-	glfwInit();
+	//获取扩展
+	void getExtensions() {
+		uint32_t extensionCount = 0;
+		//获取扩展数量
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+		//获取扩展属性
+		std::vector<VkExtensionProperties> extensions(extensionCount);
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+		//打印扩展
+		std::cout << "available extensions:" << std::endl;
+		for (const auto& extension : extensions) {
+			std::cout << '\t' << extension.extensionName << std::endl;
+		}
+	}
+private:
+	const uint32_t WIDTH = 800;
+	const uint32_t HEIGHT = 600;
+	GLFWwindow* window;
+	VkInstance instance;
 
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-	std::cout << extensionCount << " extensions supported\n";
-
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
-
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
+	//验证层
+	const std::vector<const char*> validationLayers = {
+	"VK_LAYER_KHRONOS_validation"
+	};
+	//开启验证层
+#ifdef NDEBUG
+	const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif
+	//窗口初始化
+	void initWindow() {
+		//初始化glfw
+		glfwInit();
+		//不创建opengl上下文
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		//不可调整窗口大小
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+		//创建窗口,前三个参数分别指定窗口的宽度、高度和标题。 第四个参数允许您选择性地指定要打开窗口的显示器，最后一个参数仅与 OpenGL 相关。
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
 	}
 
-	glfwDestroyWindow(window);
+	//初始化
+	void initVulkan() {
+		createInstance();
+	}
+	//主循环
+	void mainLoop() {
+		//循环检查窗口是否被关闭
+		while (!glfwWindowShouldClose(window)) {
+			//等待事件
+			glfwPollEvents();
+		}
+	}
+	//清理内存
+	void cleanup() {
+		vkDestroyInstance(instance, nullptr);
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
 
-	glfwTerminate();
+	void createInstance() {
+		//创建应用程序实例,使用列表初始化，将结构体的所有成员初始化为零，包括扩展pNext链表和其他结构体。
+		VkApplicationInfo appInfo{};
+		//指定应用程序的信息
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Hello Triangle";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No Engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+		//创建实例
+		VkInstanceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		//获取扩展
+		uint32_t glfwExtensionCount = 0;
+		const char** glfwExtensions;
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+		createInfo.enabledExtensionCount = glfwExtensionCount;
+		createInfo.ppEnabledExtensionNames = glfwExtensions;
+		//启用扩展
+		createInfo.enabledLayerCount = 0;
+		//创建实例
+		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create instance!");
+		}
+	}
+};
 
-	return 0;
+int main() {
+
+	HelloTriangleApplication app;
+	try {
+		app.getExtensions();
+		app.run();
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
 }
